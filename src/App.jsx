1fc667b011e0bacef4c74895a234ad07eafaee53
Ahmar khan <ahmarbilal223344@gmail.com>
2023-03-04 22:09:05 +0500

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Home from "./components/routes/home/Home"
 import { Routes, Route } from "react-router-dom"
 import Navigation from "./components/routes/navigation/Navigation"
+import SignIn from "./components/routes/signIn/SignIn"
 
 const Shop = () => {
   return <div>This is shop component</div>
@@ -12,6 +13,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="signIn" element={<SignIn />} />
       </Route>
     </Routes>
   )
--- src/components/routes/navigation/Navigation.jsx
+++ src/components/routes/navigation/Navigation.jsx
@@ -1,17 +1,24 @@
 import React from "react"
 import { Outlet, Link } from "react-router-dom"
 import { Fragment } from "react"
+import svg from "../../../assets/87 - crown.svg"
+import "./navigation.scss"
 
 const Navigation = () => {
   return (
     <Fragment>
       <div className="navigation">
         <Link to={"/"} className="logo-container">
-          <div>Logo</div>
+          <div>
+            <img src={svg} alt="" />
+          </div>
         </Link>
         <div className="nav-links-container">
           <Link to={"/shop"} className="nav-link">
             Shop
+          </Link>
+          <Link to={"/signIn"} className="nav-link">
+            Sign in
           </Link>
         </div>
       </div>
