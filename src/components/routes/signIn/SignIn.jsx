16003f96242c001318f1fa32a8f6ee0e3b1546e7
Ahmar khan <ahmarbilal223344@gmail.com>
2023-03-05 19:37:27 +0500

    done some changes

--- src/components/routes/signIn/SignIn.jsx
+++ src/components/routes/signIn/SignIn.jsx
@@ -1,9 +1,20 @@
+import { async } from "@firebase/util"
 import React from "react"
+import {
+  signInWithGooglePopup,
+  createUserDocFromAuth,
+} from "../../../utility/firebase/firebase"
 
 const SignIn = () => {
+  async function logGoogleUser() {
+    const { user } = await signInWithGooglePopup()
+    createUserDocFromAuth(user)
+  }
+
   return (
     <div>
       <h1>Sign in page</h1>
+      <button onClick={logGoogleUser}>Sign in with google</button>
     </div>
   )
 }
---
+++ src/utility/firebase/firebase.js
@@ -0,0 +1,36 @@
+import { initializeApp } from "firebase/app"
+import {
+  getAuth,
+  signInWithPopup,
+  signInWithRedirect,
+  GoogleAuthProvider,
+} from "firebase/auth"
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCsgoQr8H0Q1KMWuV1_GXLuWkL6hGst_bM",
+  authDomain: "commerce-web-app.firebaseapp.com",
+  projectId: "commerce-web-app",
+  storageBucket: "commerce-web-app.appspot.com",
+  messagingSenderId: "946812224271",
+  appId: "1:946812224271:web:a7e4898c597a1c87fe747a",
+  measurementId: "G-BT1JCE70KS",
+}
+
+const app = initializeApp(firebaseConfig)
+const provider = new GoogleAuthProvider()
+
+provider.setCustomParameters({
+  prompt: "select_account",
+})
+
+export const auth = getAuth()
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
+
+export const db = getFirestore()
+
+export const createUserDocFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid)
+  console.log(userDocRef)
+  const userSnapshot = await getDoc(userDocRef)
+}
